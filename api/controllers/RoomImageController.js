0c2d779fb6aaf1505585fab3c7464933972299b6
thekaroe-thailand <thekaroe@gmail.com>
2024-07-24 22:13:35 +0700

    day 28

--- api/controllers/RoomImageController.js
+++ api/controllers/RoomImageController.js
@@ -3,12 +3,59 @@ const prisma = new PrismaClient();
 
 module.exports = {
   create: async (req, res) => {
+    const fileRoom = req.files.fileRoom;
+
     try {
-      const fileRoom = req.files.fileRoom;
-      fileRoom.mv("./uploads/" + fileRoom.name, (err) => {
+      fileRoom.mv("./uploads/" + fileRoom.name, async (err) => {
         if (err) throw err;
+
+        await prisma.roomImage.create({
+          data: {
+            roomId: parseInt(req.params.roomId),
+            name: fileRoom.name,
+          },
+        });
+
         return res.send({ message: "success" });
       });
+    } catch (e) {
+      return res.status(500).send({ error: e.message });
+    }
+  },
+  list: async (req, res) => {
+    try {
+      const roomId = parseInt(req.params.roomId);
+      const results = await prisma.roomImage.findMany({
+        where: {
+          roomId: roomId,
+        },
+      });
+
+      return res.send({ results: results });
+    } catch (e) {
+      return res.status(500).send({ error: e.message });
+    }
+  },
+  remove: async (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
+      const row = await prisma.roomImage.findFirst({
+        where: {
+          id: id,
+        },
+      });
+      const imageName = row.name; // name from database
+
+      const fs = require("fs");
+      fs.unlinkSync("./uploads/" + imageName);
+
+      await prisma.roomImage.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      return res.send({ message: "success" });
     } catch (e) {
       return res.status(500).send({ error: e.message });
     }
--- api/server.js
+++ api/server.js
@@ -11,12 +11,19 @@ app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("API Working.");
 });
 app.post("/api/roomImage/create/:roomId", (req, res) =>
   RoomImageController.create(req, res)
+);
+app.delete("/api/roomImage/remove/:id", (req, res) =>
+  RoomImageController.remove(req, res)
+);
+app.get("/api/roomImage/list/:roomId", (req, res) =>
+  RoomImageController.list(req, res)
 );
 app.post("/api/user/signIn", (req, res) => UserController.signIn(req, res));
 app.get("/api/user/info", (req, res) => UserController.info(req, res));
